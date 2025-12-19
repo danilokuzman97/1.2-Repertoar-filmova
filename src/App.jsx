56a5c2c6c057e4f9fdeeba5893497d9e976467e9
Danilo <danilokuzman1@yahoo.com>
2025-12-19 18:20:09 +0100

    Repertoar filmova: Unapredjen prikaz filma, implementirane like i dislike, prikaz postera.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,43 @@ import React from "react";
 import Movie from "./Movie";
 
 const movies = [
-    {title: "Captain America - The First Avenger", hall: 2, price: 350},
-    {title: "The Papillon", hall: 1, price: 300},
-    {title: "The Lost City of Z", hall: 5, price: 350}
-  ];
+  {
+    title: "Captain America - The First Avenger",
+    hall: 2,
+    price: 350,
+    poster: "https://m.media-amazon.com/images/I/51Xp+8qDCbL._AC_UF350,350_QL50_.jpg"
+  },
+  {
+    title: "The Papillon",
+    hall: 1,
+    price: 300,
+    poster: "https://m.media-amazon.com/images/M/MV5BMjIxMTMyOTE2NF5BMl5BanBnXkFtZTgwMDYyNzY1NTM@._V1_.jpg"
+  },
+  {
+    title: "The Lost City of Z",
+    hall: 5,
+    price: 350,
+    poster: "https://m.media-amazon.com/images/M/MV5BZmU2ODIyMWItMjU3Zi00ZmVhLWIyNDAtMWE5OWU2ZDExMGFiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+  },
+  {
+    title: "Klaus",
+    hall: 3,
+    poster: "https://m.media-amazon.com/images/I/7128yjOjl9L.jpg"
+  },
+  {
+    title: "Bullet Train",
+    poster: "https://m.media-amazon.com/images/I/71INz6LX8aL._AC_UF894,1000_QL80_.jpg"
+  }
+];
+
 
 function App() {
   const date = new Date().toLocaleDateString();
+
+  const handleInput = (movieTitle, input) => {
+    alert(`Dodelili ste "${input}" za film "${movieTitle}"`);
+  };
+
   return (
     <>
       <p><strong>Repertoar za danas: {date}</strong></p>
@@ -19,7 +49,8 @@ function App() {
             title={movie.title} 
             hall={movie.hall} 
             price={movie.price}
-            image="https://pngimg.com/uploads/clapperboard/clapperboard_PNG16.png"
+            poster={movie.poster}
+            onClick={handleInput}
           />
         ))}
     </>
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -5,13 +5,23 @@ import "./Movie.css";
 const Movie = (props) => {
   return(
     <div className="container">
+     <div className="wrapper">
       <div className="image">
-        <img className="movie-image" src={props.image} alt={props.title} />
+        <img className="movie-image" src={props.poster} alt={props.title} />
       </div>
+
+      <div className="buttons">
+        <button onClick={() => props.onClick(props.title, "Like")}>Like</button>
+        <button onClick={() => props.onClick(props.title, "Dislike")}>Dislike</button>
+      </div>
+     </div>
+
       <div className="details">
-        <span>{props.title}, </span>
-        <span>Sala: {props.hall}, </span>
-        <span>Cena: {props.price} din</span>
+        <span>
+          {props.title}
+          {props.hall ? `, Sala: ${props.hall}` : " Film nije na repertoaru."} 
+        </span>
+        <span>Cena: {props.price || 300} din</span>
       </div>
     </div>
   );
